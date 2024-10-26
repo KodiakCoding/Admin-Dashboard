c06ac70a4b0d8f6f738eb05924b6d4f3baf540fe
Gerald Ferguson <kodiakcoding@gmail.com>
2024-10-26 16:24:57 -0500

    frontend and backend changes

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -24,22 +24,25 @@ import {
   useTheme,
 } from "@mui/material";
 
-const Navbar = ({
-  isSidebarOpen,
-  setIsSidebarOpen
-}) => {
-    const dispatch = useDispatch();
-    const theme = useTheme();
+const Navbar = ({ user, isSidebarOpen, setIsSidebarOpen }) => {
+  const dispatch = useDispatch();
+  const theme = useTheme();
+
+  const [anchorEl, setAnchorEl] = useState(null);
+  const isOpen = Boolean(anchorEl);
+  const handleClick = (event) => setAnchorEl(event.currentTarget);
+  const handleClose = () => setAnchorEl(null);
 
   return (
     <AppBar
-    sx={{
+      sx={{
         position: "static",
         background: "none",
         boxShadow: "none",
-      }}   
-   >
-        <Toolbar sx={{ justifyContent: "space-between" }}>
+      }}
+    >
+      <Toolbar sx={{ justifyContent: "space-between" }}>
+        {/* LEFT SIDE */}
         <FlexBetween>
           <IconButton onClick={() => setIsSidebarOpen(!isSidebarOpen)}>
             <MenuIcon />
@@ -55,10 +58,10 @@ const Navbar = ({
               <Search />
             </IconButton>
           </FlexBetween>
-        </FlexBetween>  
+        </FlexBetween>
 
-          {/* RIGHT SIDE */}
-          <FlexBetween gap="1.5rem">
+        {/* RIGHT SIDE */}
+        <FlexBetween gap="1.5rem">
           <IconButton onClick={() => dispatch(setMode())}>
             {theme.palette.mode === "dark" ? (
               <DarkModeOutlined sx={{ fontSize: "25px" }} />
@@ -68,11 +71,60 @@ const Navbar = ({
           </IconButton>
           <IconButton>
             <SettingsOutlined sx={{ fontSize: "25px" }} />
-          </IconButton> 
-        </FlexBetween>
-        </Toolbar>
-    </AppBar>
-  )
-}
+          </IconButton>
 
-export default Navbar
+          <FlexBetween>
+            <Button
+              onClick={handleClick}
+              sx={{
+                display: "flex",
+                justifyContent: "space-between",
+                alignItems: "center",
+                textTransform: "none",
+                gap: "1rem",
+              }}
+            >
+              <Box
+                component="img"
+                alt="profile"
+                src={profileImage}
+                height="32px"
+                width="32px"
+                borderRadius="50%"
+                sx={{ objectFit: "cover" }}
+              />
+              <Box textAlign="left">
+                <Typography
+                  fontWeight="bold"
+                  fontSize="0.85rem"
+                  sx={{ color: theme.palette.secondary[100] }}
+                >
+                  {user.name}
+                </Typography>
+                <Typography
+                  fontSize="0.75rem"
+                  sx={{ color: theme.palette.secondary[200] }}
+                >
+                  {user.occupation}
+                </Typography>
+              </Box>
+              <ArrowDropDownOutlined
+                sx={{ color: theme.palette.secondary[300], fontSize: "25px" }}
+              />
+            </Button>
+            <Menu
+              anchorEl={anchorEl}
+              open={isOpen}
+              onClose={handleClose}
+              anchorOrigin={{ vertical: "bottom", horizontal: "center" }}
+            >
+              <MenuItem onClick={handleClose}>Log Out</MenuItem>
+            </Menu>
+          </FlexBetween>
+        </FlexBetween>
+      </Toolbar>
+    </AppBar>
+  );
+};
+
+export default Navbar;
--- server/index.js
+++ server/index.js
@@ -11,6 +11,11 @@ import generalRoutes from "./routes/general.js";
 import managementRoutes from "./routes/management.js";
 import salesRoutes from "./routes/sales.js";
 
+//data imports
+import User from "./models/User.js";
+import {dataUser} from "./data/index.js";
+
+
 dotenv.config();
 const app = express();
 app.use(express.json());
